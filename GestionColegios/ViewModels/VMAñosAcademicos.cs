1b2b86e8c5621c984792e30430819063a31098c4
tJonh619 <tjonh619@gmail.com>
2024-10-06 22:19:29 -0600

    Modulo de Años Academicos Funcionando y ajustes en la Vista principal de Maestros

---
+++ GestionColegios/ViewModels/VMAñosAcademicos.cs
@@ -0,0 +1,15 @@
+﻿using GestionColegios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionColegios.ViewModels
+{
+    public class VMAñosAcademicos
+    {
+        public List<AñoAcademico> AñosAcademicos { get; set; }
+        public AñoAcademico AñoAcademico { get; set; }
+
+    }
+}
