a0a1b55bc8027fd4cbe87ef1ce43cd0a2f6688a5
Kennet <familiagonzalez040@gmail,com>
2024-10-30 03:06:43 -0600

    Calificaciones avances

---
+++ GestionColegios/ViewModels/VMCalificaciones.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GestionColegios.Models;
+
+namespace GestionColegios.ViewModels
+{
+    public class VMCalificaciones
+    {
+        public List<Calificacion> Calificaciones { get; set; }
+        public Calificacion Calificacion { get; set; }
+
+        public List<Estudiante> Estudiantes { get; set; }
+        public Estudiante Estudiante { get; set; }
+        public List<Materia> Materias { get; set; }
+        public Materia Materia { get; set; }
+
+        public List<Parcial> Parciales { get; set; }
+        public Parcial Parcial { get; set; }
+
+        public List<Matricula> Matriculas { get; set; }
+
+        public Matricula Matricula { get; set; }
+
+        public List<AñoAcademico> AñoAcademicos { get; set; }
+
+        public AñoAcademico AñoAcademico { get; set; }
+
+
+
+    }
+}
